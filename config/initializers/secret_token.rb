c6f539a6b8cd65c222b7b9673faafb5e1c5da857
Patrick D. Kelly <e@pdk.to>
2013-05-30 22:28:34 -1000

    initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bikerackmap::Application.config.secret_token = 'c1f316641e1192f4d6622ff8a5be004b502e84cdbf308d2e52f272b564993ae4808299b1a80117ffd7c9280a7bbd4c7d0c0d04fcfbcbca35687bad7c7736ae00'

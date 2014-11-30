8ca105905f6d0e3435a0371cd67b8424371140d2
Patrick D. Kelly <e@pdk.to>
2014-11-30 08:42:30 -1000

    gadzooks! cannot have secrets stored in public github. duh.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Bikerackmap::Application.config.secret_token = 'c1f316641e1192f4d6622ff8a5be004b502e84cdbf308d2e52f272b564993ae4808299b1a80117ffd7c9280a7bbd4c7d0c0d04fcfbcbca35687bad7c7736ae00'
+Bikerackmap::Application.config.secret_token = ENV['BIKERACKMAP_SECRET_TOKEN']
